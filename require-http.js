0f5ba5907988e9ee546226822eebb44d474f6725
verrixkio <verrixkio@gmail.com>
2019-02-11 19:44:01 +0000

    Requiring my module function :)

---
+++ require-http.js
@@ -0,0 +1,14 @@
+//var https = require('https');
+var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+function printHTML (html) {
+  console.log(html);
+}
+
+console.log(getHTML(requestOptions, printHTML))
+
